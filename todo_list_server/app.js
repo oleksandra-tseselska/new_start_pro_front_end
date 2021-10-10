02eb1b890055d9b8b46845bea4115b2259ac3268
Oleksandra Tseselska <alexiyast@gmail.com>
2021-10-10 17:28:05 +0300

    todoList server done

--- todo_list_server/app.js
+++ todo_list_server/app.js
@@ -1,29 +1,7 @@
-// Взять задачу из 13 урока (todo-list-on-steroids) и расширить ее:
-// 1. Залогиниться вот тут https://gorest.co.in/ со своим github аккаунтом
-// 2. Скопировать API Access Token в TodoApi (смотри ниже)
-// 3. При загрузке страницы показать список todo
-// 4. Добавить возможность удаления на сервере, при удалении элемента обновлять список todo на странице
-
-// Если что-то забыли смотрим код с лекции.
-
-// Задание со звездочкой (не обязательное):
-// - При изменении статуса сообщения менять его на сервере и отображать это на странице
-// - Сделать создание нового элемента на сервере и отображать это на странице
-
-
-
-// Create POST    https://gorest.co.in/public/v1/todos
-// Read   GET
-// - get list     https://gorest.co.in/public/v1/todos
-// - get one      https://gorest.co.in/public/v1/todos/1
-// Update PUT     https://gorest.co.in/public/v1/todos/1
-// Delete DELETE  https://gorest.co.in/public/v1/todos/1
-
-
-// Файл TodoAPI.js
 class TodoAPI {
   static TOKEN = '3e275c0d22591b010db7a846b6dd642f9fb9df75a252c9ab3e5497c3b16fab8a';
   static URL = 'https://gorest.co.in/public/v1/todos';
+  static USER_ID = 248;
   static HEADERS = {
     'Accept': 'application/json',
     'Content-type': 'application/json; charset=UTF-8',
@@ -31,7 +9,7 @@ class TodoAPI {
   };
 
   static getList() {
-    return fetch(this.URL)
+    return fetch(`${this.URL}?user_id=${this.USER_ID}`)
       .then((res) => {
         if(res.ok) {
           return res.json();
@@ -54,12 +32,38 @@ class TodoAPI {
     });
   }
 
-  static create(todoData) {
-    // Задача со звездочкой 1:50:43
-    return '';
+  static create(todo) {
+    return fetch(`${this.URL}`, {
+      method: 'POST',
+      headers: this.HEADERS,
+      body: JSON.stringify({
+        ...todo,
+        user_id: this.USER_ID,
+      }),
+    })
+      .then((res) => {
+        if(res.ok) {
+          return res.json();
+        }
+
+      return res.json().then((data) => {
+        throw new Error('Can not execute create todo request on API \n' + JSON.stringify(data.data, null, 4));
+      })
+    });
   }
 
-  static update(todoData) {
-    // Задача со звездочкой
+  static update(id, data) {
+    return fetch(`${this.URL}/${id}`, {
+      method: 'PUT',
+      headers: this.HEADERS,
+      body: JSON.stringify(data)
+    })
+    .then((res) => {
+      if(res.ok) {
+        return res.json();
+      }
+
+    throw new Error('Can not execute update todo request on API');
+    })
   }
 }
--- todo_list_server/todo_list_server.js
+++ todo_list_server/todo_list_server.js
@@ -1,8 +1,9 @@
-const ID_TODO = Object.freeze({
-  LIST: '#todo-list',
+const SELECTOR = Object.freeze({
+  TODO_LIST: '#todo-list',
   TEMPLATE: '#toDoListTemplate',
   BTN_ADD: '#button-add',
-  INPUT: '#input',
+  LOADING: '#loading',
+  ERROR: '#error',
 });
 
 const CLASS = Object.freeze({
@@ -10,35 +11,60 @@ const CLASS = Object.freeze({
   BTN_BIN:'btn-bin',
   TO_DO_ITEM: 'todo-item',
   DONE: 'done',
+  HIDE: 'hide',
 });
 
 const PLACEHOLDER = Object.freeze({
   TEXT: '{{text}}',
   TODO_ID: '{{todo-id}}',
+  CLASS_DONE: '{{class-done}}',
+  STATUS: '{{status}}',
 });
 
-const ul = document.querySelector(ID_TODO.LIST);
-const template = document.querySelector(ID_TODO.TEMPLATE).innerHTML;
-const btnAdd = document.querySelector(ID_TODO.BTN_ADD);
-const input = document.querySelector(ID_TODO.INPUT);
+const STATUS = Object.freeze({
+  COMPLETED: 'completed',
+  PENDING: 'pending',
+})
 
-function init() {
-  TodoAPI.getList()
-    .then((todoList) => addTodoList(todoList))
-    .catch((e) => alert(e.message))
-}
+const HELPERS = Object.freeze({
+  EMPTY_STRING: '',
+  POINT: '.',
+})
 
-init();
+const ul = document.querySelector(SELECTOR.TODO_LIST);
+const template = document.querySelector(SELECTOR.TEMPLATE).innerHTML;
+const btnAdd = document.querySelector(SELECTOR.BTN_ADD);
+const loading = document.querySelector(SELECTOR.LOADING);
+const error = document.querySelector(SELECTOR.ERROR);
 
 btnAdd.addEventListener('click', onButtonClick);
 ul.addEventListener('click', onTodoItemClick);
 
+init();
+
+function init() {
+  toggleLoading();
+
+  TodoAPI.getList()
+    .then(addTodoList)
+    .catch(handleError)
+    .finally(toggleLoading);
+}
+
 function onButtonClick() {
+  const todo = getTodo();
+
   if (isEmpty(input.value)) {
     alert('Add text, please');
     return;
   }; 
-  addItem(input);
+
+  if (isValidTodo(todo)) {
+    addTodo(todo);
+  } else {
+    alert('The text must be more than 3 characters');
+  }
+
   resetInput();
 };
 
@@ -50,51 +76,103 @@ function onTodoItemClick(e) {
     return toggleDone(todoEl);
   };
   if (classList.contains(CLASS.BTN_BIN)){
-    // return removeTodo(todoEl);
-    removeTodo(todoEl).then(() => {
-      TodoAPI.getList()
-        .then((todoList) => addTodoList(todoList))
-        .catch((e) => alert(e.message));
-    })
-
-    return;
+    return removeTodo(todoEl);
   };
 }
 
 function addTodoList(todoList) {
-  const html = todoList.map(todo => getHtml(todo)).join('');
+  const html = todoList.map(todo => getHtml(todo)).join(HELPERS.EMPTY_STRING);
 
   ul.innerHTML = html;
 }
 
 function getHtml(todo) {
   return template
+    .replace(PLACEHOLDER.CLASS_DONE, addClassDone(todo))
     .replace(PLACEHOLDER.TEXT, todo.title)
     .replace(PLACEHOLDER.TODO_ID, todo.id)
+    .replace(PLACEHOLDER.STATUS, todo.status)
 }
 
-function addItem(input) {
-  const addTextList = template.replace(PLACEHOLDER.TEXT, input.value);
-  ul.insertAdjacentHTML('beforeend', addTextList);
+function getTodo() {
+  return {
+    title: input.value,
+    status: STATUS.PENDING,
+  }
 }
 
-function getTodoElement(target) {
-  return target.closest('.' + CLASS.TO_DO_ITEM);
+function addTodo(todo) {
+  toggleLoading();
+
+  TodoAPI.create(todo)
+    .then(() => TodoAPI.getList())
+    .then(addTodoList)
+    .catch(handleError)
+    .finally(toggleLoading);
 }
 
-function toggleDone(el) {
-  el.classList.toggle(CLASS.DONE);
+function toggleDone(todoEl) {
+  toggleLoading();
+
+  const status = checkStatus(todoEl);
+  
+  return TodoAPI.update(+todoEl.dataset.id, { status })
+    .then(() => TodoAPI.getList())
+    .then(addTodoList)
+    .catch(handleError)
+    .finally(toggleLoading); 
 }
 
 function removeTodo(el) {
-  // el.remove();
+  toggleLoading();
+  
   return TodoAPI.delete(+el.dataset.id)
+    .then(() => TodoAPI.getList())
+    .then(addTodoList)
+    .catch(handleError)
+    .finally(toggleLoading); 
 }
 
-function resetInput() {
-  input.value = '';
+function getTodoElement(target) {
+  return target.closest(HELPERS.POINT + CLASS.TO_DO_ITEM);
+}
+
+function checkStatus(todoEl) {
+  if(todoEl.dataset.status === STATUS.COMPLETED) {
+    return STATUS.PENDING;
+  }
+  if(todoEl.dataset.status !== STATUS.COMPLETED) {
+    return STATUS.COMPLETED;
+  }
+}
+
+function addClassDone(todo) {
+  if(todo.status === STATUS.COMPLETED) {
+    return CLASS.DONE;
+  }
+  if(todo.status !== STATUS.COMPLETED) {
+    return HELPERS.EMPTY_STRING;
+  }
 }
 
 function isEmpty(value) {
-  return value === '';
+  return value === HELPERS.EMPTY_STRING;
+}
+
+function isValidTodo(todo) {
+  return todo && todo.title && todo.title.length >= 3;
+}
+
+function resetInput() {
+  input.value = HELPERS.EMPTY_STRING;
+}
+
+function handleError(e) {
+  error.textContent = e.message;
+
+  setTimeout(() => error.textContent = HELPERS.EMPTY_STRING, 5000);
+}
+
+function toggleLoading() {
+  loading.classList.toggle(CLASS.HIDE);
 }
